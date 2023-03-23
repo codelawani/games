821062c0076d9edd6a398fc5d15e5656a19308cb
Nico <lawani.gk@gmail.com>
2023-03-23 21:18:05 +0100

    add docstrings to settings module of ludo

--- Ludo_Game/settings.py
+++ Ludo_Game/settings.py
@@ -1,4 +1,5 @@
 class Color:
+    """This class contains the colors used in the game."""
 
     GREEN = '#0CED2C'
     RED = '#F71313'
@@ -10,6 +11,7 @@ class Color:
 
 
 class Board:
+    """This class contains the board settings."""
 
     SQUARE_SIZE = 40
     PANEL_WIDTH = 600
@@ -22,27 +24,29 @@ class Board:
 
 
 class Text:
+    """This class contains the text used in the game."""
 
     MADE_BY = 'Made By: Mansi Agrawal & Shivam Gupta'
-    HEADER =  'LUDO - THE GAME'
+    HEADER = 'LUDO - THE GAME'
 
 
 class Path:
+    """This class contains the path of the game."""
 
     def __init__(self):
-
+        """This method initializes the path of the game."""
         self.green_path = []
         self.red_path = []
         self.blue_path = []
         self.yellow_path = []
         self.gx = None
-        self.gy = None 
+        self.gy = None
         self.ry = None
         self.by = None
         self.count = None
 
     def update_coordinates(self, gx, gy, ry, by, count):
-
+        """This method updates the coordinates of the path."""
         self.gx = gx
         self.gy = gy
         self.ry = ry
@@ -50,78 +54,85 @@ class Path:
         self.count = count
 
     def start_populating(self):
-
-        #1
+        """This method populates the path of the game."""
+        # 1
         self.update_coordinates(60, 260, 540, 340, 5)
         self.direct(pow_index=0, direction='right')
-        #2
+        # 2
         self.update_coordinates(260, 220, 340, 380, 5)
         self.direct(pow_index=3, direction='up')
-        #3
+        # 3
         self.update_coordinates(260, 20, 340, 580, 3)
-        self.direct(direction='right') 
-        #4
+        self.direct(direction='right')
+        # 4
         self.update_coordinates(340, 60, 260, 540, 5)
         self.direct(pow_index=0, direction='down')
-        #5
+        # 5
         self.update_coordinates(380, 260, 220, 340, 5)
         self.direct(pow_index=3, direction='right')
-        #6
+        # 6
         self.update_coordinates(580, 260, 20, 340, 3)
         self.direct(direction='down')
-        #7
+        # 7
         self.update_coordinates(540, 340, 60, 260, 5)
         self.direct(pow_index=0, direction='left')
-        #8
+        # 8
         self.update_coordinates(340, 380, 260, 220, 5)
         self.direct(pow_index=3, direction='down')
-        #9
+        # 9
         self.update_coordinates(340, 580, 260, 20, 3)
         self.direct(direction='left')
-        #10
+        # 10
         self.update_coordinates(260, 540, 340, 60, 5)
         self.direct(pow_index=0, direction='up')
-        #11
+        # 11
         self.update_coordinates(220, 340, 380, 260, 6)
         self.direct(pow_index=3, direction='left')
-        #12
+        # 12
         self.update_coordinates(20, 300, 580, 300, 7)
         self.direct(direction='right')
 
-    def direct_horizontal(self, k, pow_index = -1):
-
+    def direct_horizontal(self, k, pow_index=-1):
+        """This method populates the path of the game in horizontal direction."""
         for i in range(self.count):
             if i == pow_index:
                 p = 1
             else:
                 p = 0
-            self.green_path.append((self.gx  +  k*i*Board.SQUARE_SIZE, self.gy, p))
-            self.red_path.append((self.gy, self.ry  -  k*i*Board.SQUARE_SIZE, p))
-            self.blue_path.append((self.ry - k*i*Board.SQUARE_SIZE, self.by, p))
-            self.yellow_path.append((self.by, self.gx + k*i*Board.SQUARE_SIZE, p))
+            self.green_path.append(
+                (self.gx + k*i*Board.SQUARE_SIZE, self.gy, p))
+            self.red_path.append((self.gy, self.ry - k*i*Board.SQUARE_SIZE, p))
+            self.blue_path.append(
+                (self.ry - k*i*Board.SQUARE_SIZE, self.by, p))
+            self.yellow_path.append(
+                (self.by, self.gx + k*i*Board.SQUARE_SIZE, p))
 
-    def direct_vertical(self, k, pow_index = -1):
-
+    def direct_vertical(self, k, pow_index=-1):
+        """This method populates the path of the game in vertical direction."""
         for i in range(self.count):
             if i == pow_index:
                 p = 1
             else:
                 p = 0
-            self.green_path.append((self.gx, self.gy - k*i*Board.SQUARE_SIZE, p))
-            self.red_path.append((self.gy - k*i*Board.SQUARE_SIZE,self.ry, p))
-            self.blue_path.append((self.ry, self.by + k*i*Board.SQUARE_SIZE, p))
-            self.yellow_path.append((self.by + k*i*Board.SQUARE_SIZE, self.gx, p))
+            self.green_path.append(
+                (self.gx, self.gy - k*i*Board.SQUARE_SIZE, p))
+            self.red_path.append((self.gy - k*i*Board.SQUARE_SIZE, self.ry, p))
+            self.blue_path.append(
+                (self.ry, self.by + k*i*Board.SQUARE_SIZE, p))
+            self.yellow_path.append(
+                (self.by + k*i*Board.SQUARE_SIZE, self.gx, p))
 
-
-    def direct(self, direction, pow_index = -1):
-        if direction=='right':
+    def direct(self, direction, pow_index=-1):
+        """This method populates the path of the game in any direction."""
+        if direction == 'right':
             self.direct_horizontal(1, pow_index=pow_index)
-        elif direction=='left':
+        elif direction == 'left':
             self.direct_horizontal(-1, pow_index=pow_index)
-        elif direction=='down':
+        elif direction == 'down':
             self.direct_vertical(-1, pow_index=pow_index)
         else:
             self.direct_vertical(1, pow_index=pow_index)
+
 
 path = Path()
 path.start_populating()
